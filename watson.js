6b1fe17b427b40d445e09ffa737440148c21d217
Cameron Sanders <cameronsanders@utexas.edu>
2019-10-22 12:36:52 -0500

    commit for interview

--- watson.js
+++ watson.js
@@ -1,3 +1,5 @@
+//Cameron Sanders - challenge submission 
+
 /*
 You are given a positive integer N, and two single-digit integers p and q, where p != q and both are greater than 1. You must output all of the integers from 1 to N inclusive, separated by a single space.
 However, any integer divisible by p whose decimal representation does not contain the digit p should be replaced by the text WAT and any integer divisible by q whose decimal representation does not contain the digit q should be replaced by the text SON. Integers for which both of the preceding statements are true should instead be replaced by the text WATSON. Please implement using Node.js and with the mindset that this will be production level code that will be supported by you and your team.
@@ -6,7 +8,6 @@ input N p q
 */
 
 var readline = require('readline');
-//var rl = require('rl');
 
 const readInterface = readline.createInterface({
     input: process.stdin,
